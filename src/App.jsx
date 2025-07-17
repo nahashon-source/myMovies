267eb99b71afe5891186e53f74e22d594c12c1cb
nahashon <nashonmwendwa0@gmail.com>
2025-07-17 13:59:56 +0300

    Add React Router setup for routing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* More routes will be added here */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
